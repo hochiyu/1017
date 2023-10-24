5594d472a0eaefc86cfc4c43798c9a6b80034a63
hochiyu <ho.trina0609@gmail.com>
2023-10-24 16:26:50 +0800

    firebase

--- src/App.vue
+++ src/App.vue
@@ -5,25 +5,32 @@ import ChemistryRadio from "./components/ChemistryRadio.vue";
 import ChemistryCheckbox from "./components/ChemistryCheckbox.vue";
 import EnglishCheckbox from "./components/EnglishCheckbox.vue";
 import EnglishRadio from "./components/EnglishRadio.vue";
-import { ref } from "vue";
-let choice = ref("請選擇");
+import English from "./components/EnglishDB.vue";
+import { reactive } from "vue";
+let choice = reactive({title:'化學', value:"Chemistry"});
+let items=[
+  {title:'英文', value:"English"},
+  {title:'化學', value:"Chemistry"},
+  {title:'化學單選', value:"ChemistryRadio"},
+  {title:'化學複選', value:"ChemistryCheckbox"},
+  {title:'英文單選', value:"EnglishRadio"},
+  {title:'英文複選', value:"EnglishCheckbox"}
+  ]
 </script>
 
 <template>
   <div>
-    <select v-model="choice">
-        <option value="Add">加法</option>
-      <option value="Chemistry">化學填空</option>
-      <option value="ChemistryRadio">化學單選</option>
-      <option value="ChemistryCheckbox">化學複選</option>
-      <option value="EnglishCheckbox">英文複選</option>
-      <option value="EnglishRadio">英文單選</option>
-    </select>
-    <Add v-if="choice === 'Add'" />
-    <Chemistry v-else-if="choice === 'Chemistry'" />
-    <ChemistryRadio v-else-if="choice === 'ChemistryRadio'"/>
-    <ChemistryCheckbox v-else-if="choice === 'ChemistryCheckbox'" />
-    <EnglishCheckbox v-else-if="choice === 'EnglishCheckbox'" />
-    <EnglishRadio v-else/>
+    <v-select label="請選擇" v-model="choice.value" :items="items" item-title="title" item-value="value">
+    </v-select>
+    <Suspense>
+    <Add v-if="choice.value === 'Add'" />
+    <Chemistry v-else-if="choice.value === 'Chemistry'" />
+    <ChemistryRadio v-else-if="choice.value === 'ChemistryRadio'"/>
+    <ChemistryCheckbox v-else-if="choice.value === 'ChemistryCheckbox'" />
+    <EnglishCheckbox v-else-if="choice.value === 'EnglishCheckbox'" />
+    <EnglishRadio v-else-if="choice.value === 'EnglishRadio'" />
+    <English v-else/>
+  </Suspense>
+
   </div>
 </template>
---
+++ src/components/settings/FirebaseConfig.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCDbA22tAje1H2VDaVWUpIlJDZhaU3dU_A",
+  authDomain: "project-110683609457286038.firebaseapp.com",
+  projectId: "project-110683609457286038",
+  storageBucket: "project-110683609457286038.appspot.com",
+  messagingSenderId: "611257844421",
+  appId: "1:611257844421:web:aee74f550b9b16de05a6ac",
+  measurementId: "G-F0YS9MRBTF",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
+</script>
